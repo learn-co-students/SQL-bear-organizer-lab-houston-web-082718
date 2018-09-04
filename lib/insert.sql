858bcfe29136ea4f3996cb50cc70a5b5e00e9623
chelsme <chelsea.crowson@me.com>
2018-09-04 14:54:33 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 12, 'm', 'brown', 'happy', 1);
+INSERT INTO bears VALUES (2, 'Rowdy', 12, 'f', 'brown', 'happy', 0);
+INSERT INTO bears VALUES (3, 'Tabitha', 12, 'f', 'brown', 'happy', 0);
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 12, 'm', 'brown', 'happy', 0);
+INSERT INTO bears VALUES (5, 'Melissa', 12, 'f', 'brown', 'happy', 1);
+INSERT INTO bears VALUES (6, 'Grinch', 12, 'm', 'brown', 'happy', 1);
+INSERT INTO bears VALUES (7, 'Wendy', 12, 'm', 'brown', 'happy', 1);
+INSERT INTO bears VALUES (8, null, 12, 'f', 'brown', 'happy', 1);
